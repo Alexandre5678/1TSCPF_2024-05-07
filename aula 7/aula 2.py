6c7db5e9d35323aea67f7acf879a745a60a4fe99
Alexandre <alexandre.carvalho.goncalves@gmail.com>
2024-05-07 10:38:00 -0300

    versao inicial contagem em vogais

---
+++ aula 7/aula 2.py
@@ -0,0 +1,14 @@
+frase = input('Entre com a frase para contar vogais:')
+
+vogais, consoantes, espacos = 0, 0, 0
+for palavra in frase.split():
+    print(palavra)
+for letra in frase:
+    if letra in 'aeiouAEIOUáéíóúâêôãõ':
+        vogais += 1
+    elif letra == ' ':
+        espaco += 1
+    else:
+        consoantes += 1
+
+print(f'A frase:{frase} tem {vogais} vogais, {consoantes} consoantes, {espacos} espacos')
